37420fe1c39db97020e967ab3617e87e3769be2a
capoolebugchat <anhquan.le52@gmail.com>
2022-07-29 15:10:04 +0700

    m

---
+++ KWSTransformer/transformer.py
@@ -0,0 +1,4 @@
+# Transformer class and light server initialization for simple TF model
+# This files includes: Transformer class def: preprocessing, postprocessing
+# along with: Transformer server initialization
+
